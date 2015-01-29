c6e324827867508bc484dfcb2781c0dd12ca312e
Zane Rockenbaugh <zane@liquid-labs.com>
2015-01-29 04:48:28 +0000

    always a rec

--- pkgs/servers/sql/conveyor-mysql/default.nix
+++ pkgs/servers/sql/conveyor-mysql/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, perl, pkgconfig, gcc, openssl, zlib, cmake, bison, ncurses, patchelf }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   inherit perl;
 
   domain_name = "dogfoodsoftware.com";
